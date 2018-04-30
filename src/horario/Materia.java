efdde6907bacc518560340de2d8297b6e38d8697
nirogu <nirogu@outlook.com>
2018-04-30 15:29:34 -0500

    Se termina la clase materia, con setters y getters

--- src/horario/Materia.java
+++ src/horario/Materia.java
@@ -1,7 +1,31 @@
 package horario;
 
 public class Materia {
-	public static String nombre;
-	public static Dia dia;
-	public static int hora;
+	private static String nombre;
+	private static Dia dia;
+	private static int hora;
+	
+	public void setNombre (String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setDia (Dia dia) {
+		this.dia = dia;
+	}
+	
+	public void setHora (int hora) {
+		this.hora = hora;
+	}
+	
+	public String getNombre () {
+		return nombre;
+	}
+	
+	public Dia getDia () {
+		return dia;
+	}
+	
+	public int getHora () {
+		return hora;
+	}
 }
